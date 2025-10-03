fe35f965debbed9562fb37254ea84c9eaf686e12
Soliman Ali <72981030+soliman141119@users.noreply.github.com>
2025-10-03 20:05:45 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,11 +10,10 @@ variable "ami" {
   default = "ami-0669b163befffbdfc" # Ubuntu 22.04 LTS Frankfurt
 }
 
-variable "key_path" {
-  default = "/home/soliman/.ssh/soly.pub"
-}
+
 
 variable "public_key" {
-  description = "SSH public key for EC2 instances"
+  description = "Public SSH key for EC2 access"
   type        = string
 }
+
