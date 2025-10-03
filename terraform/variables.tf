6d29f22ecefe27aa8174071ccd83c0836282b693
Soliman Ali <72981030+soliman141119@users.noreply.github.com>
2025-10-03 20:32:15 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,3 @@ variable "ami" {
   default = "ami-0669b163befffbdfc" # Ubuntu 22.04 LTS Frankfurt
 }
 
-
-
-variable "public_key" {
-  description = "Public SSH key for EC2 access"
-  type        = string
-}
-
