1089b91198c86eb730e6230bdf29370ce0d63e79
Soliman Ali <72981030+soliman141119@users.noreply.github.com>
2025-10-03 20:36:23 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,14 @@ variable "ami" {
   default = "ami-0669b163befffbdfc" # Ubuntu 22.04 LTS Frankfurt
 }
 
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
